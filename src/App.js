cfa2a823a9f1f651db31b522f38bfce41a8fe682
samib93 <sami.barkaoui93290@gmail.com>
2024-12-18 13:53:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Hello, React avec Tailwind CSS !
+      </h1>
+    </div>
+  );
+}
+
+export default App;
